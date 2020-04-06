b3120770c67842bd0df6a7319f3092e4fcaa58b1
Jared Massey <jared@jaredmassey.com>
2020-04-06 12:35:52 -0600

    Finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text(
+          'Magic 8 Ball',
+          style: TextStyle(
+            color: Colors.teal,
+          ),
+        ),
+        backgroundColor: Colors.black54,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  var rng = Random();
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image.asset('images/ball$ballNumber.png'),
+        onPressed: () {
+          setState(() {
+            ballNumber = rng.nextInt(5) + 1;
+          });
+        },
+      ),
+    );
+  }
+}
